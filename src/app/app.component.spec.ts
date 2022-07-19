71d50d0c972635b5cecd14cfaf036496201474eb
Abhishek SAINI <saini.abhishek@hotmail.com>
2022-07-19 15:51:37 +0200

    change to angular standalone

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-standalone-components'`, () => {
+  it(`should have as title 'angular-standalone'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-standalone-components');
+    expect(app.title).toEqual('angular-standalone');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-standalone-components app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-standalone app is running!');
   });
 });
