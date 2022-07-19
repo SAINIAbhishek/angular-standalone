076a5d938e3570879bd14d917bb4ee1dd43af0fd
Abhishek SAINI <saini.abhishek@hotmail.com>
2022-07-19 16:14:13 +0200

    Standalone Root Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import {AppRoutingModule} from "./app-routing.module";
+import {WelcomeComponent} from "./welcome/welcome.component";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [AppRoutingModule, WelcomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
